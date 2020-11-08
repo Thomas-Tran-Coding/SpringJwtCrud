4f9e221a62099a3c42c3e7bd49fe217c4359a380
Thomas Tran <tran.thomas559@yahoo.de>
2020-11-08 21:02:23 +0100

    Added user authentication and jwt creation

--- JwtAuthCrud2/src/main/java/com/Thomas/JwtAuthCrud/config/SecurityConfigurer.java
+++ JwtAuthCrud2/src/main/java/com/Thomas/JwtAuthCrud/config/SecurityConfigurer.java
@@ -4,7 +4,9 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -29,4 +31,19 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter  {
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/authenticate").permitAll()
+			.anyRequest().authenticated();
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManager();
+	}
+	
 }
